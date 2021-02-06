69ff3b55209611c9e502849432e9c45a898200f9
William Krug <william.p.krug@gmail.com>
2021-02-05 19:47:28 -0600

    capturing all inputs from DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,21 +2,39 @@ console.log('JavaScript is ready');
 $(document).ready(onReady);
 
 const verbose = true;
+let operation = '';
 
 function onReady() {
   console.log('jQuery is ready');
   $('#calculatorInput').on('submit', calculate);
+  $('.operationButton').on('click', setOperation);
 }
 
 function calculate(event) {
   event.preventDefault();
   const newEquation = {
     firstNumber: Number($('#firstNumberInput').val()),
-    operation: '',
+    operation: operation,
     secondNumber: Number($('#secondNumberInput').val()),
   };
   if (verbose) {
     console.log('in calculate()');
     console.log('\tnewEquation:', newEquation);
+  }
+}
+
+/**
+ * Function sets the mathematical operation from user input
+ *
+ * Assistance by John Shands on setup
+ * @param {*} event
+ */
+function setOperation(event) {
+  event.preventDefault();
+  operation = $(this).data('operation');
+  if (verbose) {
+    console.log('in setOperation()');
+    console.log('\tthis:', $(this).data('operation'));
+    console.log('\toperation:', operation);
   }
 }
